e057478211e9ad2429aa209654a8bbd35055ca62
lgbouma <bouma.luke@gmail.com>
2022-02-24 16:17:50 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='agecat',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Catalog of accurate and precise exoplanet ages.',
+    author='Luke Bouma',
+    license='MIT',
+)
